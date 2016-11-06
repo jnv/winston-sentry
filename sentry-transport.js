cc7748d44eb5872f0a0ffcc53ab262319d1defad
Jan Vlnas <git@jan.vlnas.cz>
2016-11-06 22:51:35 +0100

    Capture errors using Winston's handler

--- sentry-transport.js
+++ sentry-transport.js
@@ -4,7 +4,7 @@ var util = require('util'),
     _ = require('lodash');
 
 var Sentry = winston.transports.Sentry = function (options) {
-  winston.Transport.call(this, _.pick(options, ["level", "silent"]));
+  winston.Transport.call(this, _.pick(options, ["level", "silent", "handleExceptions", "exceptionsLevel"]));
 
   // Default options
   this.defaults = {
@@ -21,7 +21,7 @@ var Sentry = winston.transports.Sentry = function (options) {
     },
     tags: {},
     extra: {}
-  }
+  };
 
   // For backward compatibility with deprecated `globalTags` option
   options.tags = options.tags || options.globalTags;
@@ -35,10 +35,10 @@ var Sentry = winston.transports.Sentry = function (options) {
   }
 
   // Handle errors
-  this._sentry.on('error', function(error) {
+  this._sentry.on("error", function(error) {
     var message = "Cannot talk to sentry.";
     if(error && error.reason) {
-        message += " Reason: " + error.reason;
+      message += " Reason: " + error.reason;
     }
     console.log(message);
   });
@@ -55,25 +55,19 @@ util.inherits(Sentry, winston.Transport);
 
 //
 // Expose the name of this Transport on the prototype
-Sentry.prototype.name = 'sentry';
+Sentry.prototype.name = "sentry";
 //
 
-Sentry.prototype.log = function (level, msg, meta, callback) {
-  if (this.silent) {
-    return callback(null, true);
-  }
-
-  level = this.options.levelsMap[level];
+Sentry.prototype._extra = function (level, meta) {
   meta = meta || {};
-
   var extraData = _.extend({}, meta),
-      tags = extraData.tags;
+    tags = extraData.tags;
   delete extraData.tags;
 
   var extra = {
-    'level': level,
-    'extra': extraData,
-    'tags': tags
+    level: this.options.levelsMap[level],
+    extra: extraData,
+    tags: tags
   };
 
   if (extraData.request) {
@@ -86,21 +80,34 @@ Sentry.prototype.log = function (level, msg, meta, callback) {
     delete extraData.user;
   }
 
-  try {
-    if(level == 'error') {
-      // Support exceptions logging
-      if (meta instanceof Error) {
-        if (msg == '') {
-          msg = meta;
-        } else {
-          meta.message = msg + ". cause: " + meta.message;
-          msg = meta;
-        }
-      }
+  return extra;
+};
 
-      this._sentry.captureException(msg, extra, function() {
-        callback(null, true);
-      });
+Sentry.prototype._captureError = function (err, msg, meta, callback) {
+  var extra = _.extend({}, meta);
+
+  if (err instanceof Error) {
+    if (msg) {
+      extra.message = msg + ". cause: " + extra.message;
+      msg = err;
+    }
+  }
+
+  this._sentry.captureException(msg, extra, function() {
+    callback(null, true);
+  });
+};
+
+Sentry.prototype.log = function (level, msg, meta, callback) {
+  if (this.silent) {
+    return callback(null, true);
+  }
+
+  var extra = this._extra(level, meta);
+
+  try {
+    if (extra.level == "error") {
+      this._captureError(meta, msg, extra, callback);
     } else {
       this._sentry.captureMessage(msg, extra, function() {
         callback(null, true);
@@ -109,6 +116,12 @@ Sentry.prototype.log = function (level, msg, meta, callback) {
   } catch(err) {
     console.error(err);
   }
+};
+
+Sentry.prototype.logException = function (msg, meta, callback, err) {
+  var extra = this._extra(this.exceptionsLevel, meta);
+
+  this._captureError(err, msg, extra, callback);
 };
 
 module.exports = Sentry;
